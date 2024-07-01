0a9c74cc9357fb54aeedb8a0f38f22995ff7e611
William Yuan <66759156+Wi11iamYuan@users.noreply.github.com>
2024-06-30 22:27:36 -0700

    was able to run locally, but still issues
    some bugs such as, cannot see history of past epochs, cannot see accurcay, running cifar10 instead of 100 due out of bound errors, summary not working, downloads the data locally, keeps local log, missing comments, needs clean up and documentation

--- train_cnn_cifar/torch-train-cnn-cifar.py
+++ train_cnn_cifar/torch-train-cnn-cifar.py
@@ -6,12 +6,11 @@ import time
 
 import torch
 from torch import nn
-import torch.nn.functional as F
-from torchvision.datasets import CIFAR100
+from torchvision.datasets import CIFAR100, CIFAR10
 from torchvision.transforms import ToTensor
 from torch.utils.data import TensorDataset, DataLoader
 import torchsummary
-from torchmetrics.functional import accuracy
+from torchmetrics import Accuracy
 import lightning as pl
 import numpy as np
 
@@ -32,145 +31,118 @@ def get_command_arguments():
     args = parser.parse_args()
     return args
 
-#%%
-class CNN(pl.LightningModule):
-    def __init__(self, classes):
-        super(CNN, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3)
-        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3)
-        self.conv3 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3)
-        self.fc1 = nn.Linear(64 * 6 * 6, 64)
-        self.fc2 = nn.Linear(64, classes)
-        self.loss_fn = nn.CrossEntropyLoss()
-
-    def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.max_pool2d(x, 2)
-        x = F.relu(self.conv2(x))
-        x = F.max_pool2d(x, 2)
-        x = F.relu(self.conv3(x))
-        x = F.max_pool2d(x, 2)
-        x = x.view(x.size(0), -1)
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        return x
-
-    def training_step(self, batch, batch_idx):
-        x, y = batch
-        logits = self(x)
-        loss = self.loss_fn(logits, y)
-        acc = accuracy(logits, y)
-        self.log('train_loss', loss)
-        self.log('train_acc', acc)
-        return loss
-
-    def validation_step(self, batch, batch_idx):
-        x, y = batch
-        logits = self(x)
-        loss = self.loss_fn(logits, y)
-        acc = accuracy(logits, y)
-        self.log('val_loss', loss)
-        self.log('val_acc', acc)
-
-    def configure_optimizers(self):
-        return torch.optim.Adam(self.parameters(), lr=1e-3)
-    
-
-class CIFARDATA(pl.LightningDataModule):
-    def __init__(self, batch_size, classes=100):
-        super().__init__()
-        self.batch_size = batch_size
-        self.classes = classes
-        self.transform = ToTensor()
-
-    def prepare_data(self):
-        # Download data if needed
-        CIFAR100(root='./data', train=True, download=True)
-        CIFAR100(root='./data', train=False, download=True)
-
-    def setup(self, stage=None):
-        # Transform data and prepare train/test datasets
-        if self.classes == 100:
-            self.train_dataset = CIFAR100(root='./data', train=True, transform=self.transform)
-            self.test_dataset = CIFAR100(root='./data', train=False, transform=self.transform)
-        elif self.classes == 20:
-            # Placeholder for CIFAR-20
-            pass
-        else:  # classes == 10
-            self.train_dataset = CIFAR100(root='./data', train=True, transform=self.transform)
-            self.test_dataset = CIFAR100(root='./data', train=False, transform=self.transform)
-
-    def train_dataloader(self):
-        return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True)
-
-    def val_dataloader(self):
-        return DataLoader(self.test_dataset, batch_size=self.batch_size)
-                                
+#%%               
 def create_datasets(classes, dtype):
     """ Create CIFAR training and test datasets """
 
     # Download training and test image datasets
     #TODO: find a way for CIFAR-20 and fine/coarse labels
-    #ToTensor() converts to 0.0-1.0, float32
-    transform = ToTensor()
-
+    #NOTE: DOES NOT WORK WITH CIFAR100, "target out of bounds error"
     if classes == 100:
-        train_dataset = CIFAR100(root='./data', train=True, transform=transform, download=True)
-        test_dataset = CIFAR100(root='./data', train=False, transform=transform, download=True)
+        train_dataset = CIFAR10(root='./data', transform=ToTensor(), train=True, download=True)
+        test_dataset = CIFAR10(root='./data', transform=ToTensor(), train=False, download=True)
+        # cifar_dataset = pl.LightningDataModule.from_datasets(train_dataset=CIFAR100(root='./data', train=True, transform=ToTensor(), download=True),
+                                                            #  test_dataset=CIFAR100(root='./data', train=False, transform=ToTensor(), download=True))
     elif classes == 20:
         # CIFAR-20 is not directly available in torchvision, this is a placeholder
         pass
     else: # classes == 10
-        train_dataset = CIFAR100(root='./data', train=True, transform=transform, download=True)
-        test_dataset = CIFAR100(root='./data', train=False, transform=transform, download=True)
+        train_dataset = CIFAR10(root='./data', transform=ToTensor(), train=True, download=True)
+        test_dataset = CIFAR10(root='./data', transform=ToTensor(), train=False, download=True)
 
     #Converting to respective tensors for analysis and building datasets
-    x_train, y_train = torch.tensor(data=train_dataset.data, dtype=dtype), torch.tensor(data=train_dataset.targets, dtype=torch.uint8)
-    x_test, y_test = torch.tensor(data=test_dataset.data, dtype=dtype), torch.tensor(data=test_dataset.targets, dtype=torch.uint8)
+    x_train, y_train = np.array(train_dataset.data), np.array(train_dataset.targets)
+    x_test, y_test = np.array(test_dataset.data), np.array(test_dataset.targets)
 
     # Verify training and test image dataset sizes
-    # assert x_train.size == (50000, 32, 32, 3)
-    # assert y_train.size == (50000, 1)
-    # assert x_test.size == (10000, 32, 32, 3)
-    # assert y_test.size == (10000, 1)
+    assert x_train.shape == (50000, 32, 32, 3)
+    assert y_train.shape == (50000,)
+    assert x_test.shape == (10000, 32, 32, 3)
+    assert y_test.shape == (10000,)
 
+    # Normalize the 8-bit (3-channel) RGB image pixel data between 0.0 
+    # and 1.0; also converts datatype from numpy.uint8 to numpy.float64
+    x_train = x_train / 255.0
+    x_test = x_test / 255.0
 
+    # Convert from NumPy arrays to PyTorch tensors
+    x_train = torch.tensor(data=x_train, dtype=dtype).permute(0, 3, 1, 2)
+    y_train = torch.tensor(data=y_train, dtype=torch.uint8)
+    x_test = torch.tensor(data=x_test, dtype=dtype).permute(0, 3, 1, 2)
+    y_test = torch.tensor(data=y_test, dtype=torch.uint8)
 
-    # Construct TensorFlow datasets
+    # Construct PyTorch datasets
     train_dataset = TensorDataset(x_train, y_train)
     test_dataset = TensorDataset(x_test, y_test)
 
     return train_dataset, test_dataset
 
 #%%
-# def create_model(classes):
-#     """ Specify and compile the CNN model """
+class CNN(pl.LightningModule):
+    def __init__(self, classes):
+        super(CNN, self).__init__()
 
-#     #NOTE: could not find InputLayer equivalent in torch, so assume that the size is 32x32, input channels specified in first Conv2d
-#     #NOTE: torch does not have activation incorporated, have to call seperately
-#     #NOTE: torch equivalent of Dense layer is Linear
-#     model = nn.Sequential(
-#         nn.Conv2d(in_channels=3, filters=32, kernel_size=3),
-#         nn.ReLU(),
-#         nn.MaxPool2d(kernel_size=2),
-#         nn.Conv2d(filters=64, kernel_size=3),
-#         nn.ReLU(),
-#         nn.MaxPool2d(kernel_size=2),
-#         nn.Conv2d(filters=64, kernel_size=3),
-#         nn.ReLU(),
-#         nn.Flatten(),
-#         nn.Linear(64,64),
-#         nn.ReLU(),
-#         nn.Linear(64,classes)
-#     )
+        self.train_acc = Accuracy(num_classes=classes, task='MULTICLASS')
+        self.valid_acc = Accuracy(num_classes=classes, task='MULTICLASS')
+        self.test_acc = Accuracy(num_classes=classes, task='MULTICLASS')
 
-#     model.compile(
-#         optimizer=torch.optim.Adam(),
-#         loss=nn.CrossEntropyLoss(),
-#         metrics=['accuracy'],
-#     )
+        self.cnn_block = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3),
+            nn.ReLU(),
+            nn.Flatten(),
+            nn.Linear(64 * 4 * 4, 64),
+            nn.ReLU(),
+            nn.Linear(64, classes)
+        )
 
-#     return model
+    def forward(self, x):
+        return self.cnn_block(x)
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+        loss = nn.functional.cross_entropy(logits, y)
+        preds = torch.argmax(logits, dim=1)
+        self.train_acc.update(preds, y)
+        self.log("train_loss", loss, prog_bar=True)
+        return loss
+
+    def on_train_epoch_end(self):
+        self.log("train_acc", self.train_acc.compute())
+        self.train_acc.reset()
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+        loss = nn.functional.cross_entropy(logits, y)
+        preds = torch.argmax(logits, dim=1)
+        self.valid_acc.update(preds, y)
+        self.log("valid_loss", loss, prog_bar=True)
+        return loss
+
+    def on_validation_epoch_end(self):
+        self.log("valid_acc", self.valid_acc.compute(), prog_bar=True)
+        self.valid_acc.reset()
+
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+        loss = nn.functional.cross_entropy(logits, y)
+        preds = torch.argmax(logits, dim=1)
+        self.test_acc.update(preds, y)
+        self.log("test_loss", loss, prog_bar=True)
+        self.log("test_acc", self.test_acc.compute(), prog_bar=True)
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=0.001)
+        return optimizer
 
 #%%
 def main():
@@ -197,21 +169,23 @@ def main():
 
     # Prepare the datasets for training and evaluation
     #TODO: cache
-    data = CIFARDATA(batch_size, classes)
+    train_dataset = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_dataset = DataLoader(test_dataset, batch_size=batch_size)
+    cifar_dataset = pl.LightningDataModule.from_datasets(train_dataset=train_dataset, test_dataset=test_dataset)
 
     # Create model
     model = CNN(classes)
 
     # Print summary of the model's network architecture
-    torchsummary.summary(model, input_size=(32, 32, 3))
+    # torchsummary.summary(model, input_size=(3, 32, 32))
 
-    # Train the model on the dataset
+    # # Train the model on the dataset
     trainer = pl.Trainer(max_epochs=epochs)
-    trainer.fit(model, datamodule=data)
+    trainer.fit(model, datamodule=cifar_dataset)
     
-    # trainer.test(model, test_dataloaders = test_dataset)
+    # # trainer.test(model, test_dataloaders = test_dataset)
 
-    trainer.save_checkpoint('saved_model_'+"""os.environ['SLURM_JOB_ID']""" "hello" + ".ckpt")
+    # trainer.save_checkpoint('saved_model_'+"""os.environ['SLURM_JOB_ID']""" "hello" + ".ckpt")
 
     return 0
 
