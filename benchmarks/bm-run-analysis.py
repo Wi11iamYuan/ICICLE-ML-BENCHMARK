e2f2d0fb8be7bcb2debbf30f419f630898ab08ac
William Yuan <66759156+Wi11iamYuan@users.noreply.github.com>
2024-07-11 11:17:52 -0700

    test

--- benchmarks/bm-run-analysis.py
+++ benchmarks/bm-run-analysis.py
@@ -9,6 +9,8 @@ import time
 for cpus in range(2, 128, 4):
     start = time.time()
     process = subprocess.Popen(["sbatch", "tf2-train-cnn-cifar-v1-bm-1.sh", "--export=ALL", f"CPUS={cpus}"], shell=True)
+    while process.poll() is None:
+        pass
     end = time.time()
     print(f"CPUS: {cpus} Time Elapsed: {end-start}")
     
