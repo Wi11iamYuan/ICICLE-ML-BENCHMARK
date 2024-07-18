c96220c3b2fb85122a15d34231db5d50ecd4869f
William Yuan <66759156+Wi11iamYuan@users.noreply.github.com>
2024-07-18 14:16:53 -0700

    cleaned

--- preprocess_imagenet_dataset/process-dataset-tf.py
+++ preprocess_imagenet_dataset/process-dataset-tf.py
@@ -1,12 +1,12 @@
-import tensorflow
+import keras
 
 
 def createdataset(root: str):
-    rawds = tensorflow.keras.utils.image_dataset_from_directory(
+    rawds = keras.utils.image_dataset_from_directory(
         root,
         image_size=(128, 192),
         seed=6059
     )
-    trainds, testvalds = tensorflow.keras.utils.split_dataset(rawds, left_size=0.7, right_size=0.3)
-    testds, valds = tensorflow.keras.utils.split_dataset(testvalds, left_size=(2 / 3), right_size=(1 / 3))
+    trainds, testvalds = keras.utils.split_dataset(rawds, left_size=0.7, right_size=0.3)
+    testds, valds = keras.utils.split_dataset(testvalds, left_size=(2 / 3), right_size=(1 / 3))
     return trainds, testds, valds
