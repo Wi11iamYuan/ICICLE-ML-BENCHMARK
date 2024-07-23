d6c9bf91d6480fe640424bf2016470d65a5f6bc6
William Yuan <66759156+Wi11iamYuan@users.noreply.github.com>
2024-07-23 10:39:17 -0700

    run on compute node rather than shared

--- benchmarks/run-tensorflow-cpu-benchmark.sh
+++ benchmarks/run-tensorflow-cpu-benchmark.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=run-tensorflow-cpu-benchmark
 #SBATCH --account=ddp324
 #SBATCH --clusters=expanse
-#SBATCH --partition=shared
+#SBATCH --partition=compute
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=8
